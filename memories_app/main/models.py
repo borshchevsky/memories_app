ffb088f7d4593a590ed56173fc79c29ca84da111
Pavel <3410914@gmail.com>
2021-05-03 21:23:15 +0300

    passing coordinates and saving to model added

--- memories_app/main/models.py
+++ memories_app/main/models.py
@@ -6,5 +6,8 @@ class Memory(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    lon = models.CharField(max_length=255)
     lat = models.CharField(max_length=255)
+    lon = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user} {self.title}'
--- memories_app/main/views.py
+++ memories_app/main/views.py
@@ -1,12 +1,34 @@
+from django.contrib.auth.models import User
+from django.db import models
 from django.shortcuts import render, redirect
+
+from .models import Memory
 
 
 def index(request):
-    return render(request, 'index.html')
+    if not request.user.is_authenticated:
+        return render(request, 'index.html')
+    user = User.objects.get(username=request.user)
+    try:
+        memories = Memory.objects.all().filter(user=user)
+    except models.ObjectDoesNotExist:
+        memories = None
+    return render(request, 'index.html', {'memories': memories})
 
 
 def get_post(request):
+    user = User.objects.get(username=request.user)
     title = request.POST.get('title')
-    text = request.POST.get('title')
-    Memory.objects.create()
+    description = request.POST.get('description')
+    coordinates = request.POST.get('coords')
+    print(type(coordinates))
+    print(f'Coords:{coordinates}')
+    lat, lon = coordinates.split(',')
+    Memory.objects.create(
+        user=user,
+        title=title,
+        description=description,
+        lat=lat,
+        lon=lon,
+    )
     return redirect('index')
