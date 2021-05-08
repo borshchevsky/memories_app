f21bbf282b79fa4196146c4d4124de4d84024d59
Pavel <3410914@gmail.com>
2021-05-08 13:11:06 +0300

    added gunicorn and nginx

--- memories_app/memories_app/settings.py
+++ memories_app/memories_app/settings.py
@@ -19,12 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#v!nelj-d*andqmpx#*1j3*qb11v9h_17j^eyi)-4b4j%&#%6('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['borshchevsky.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'borshchevsky.com']
 
 # Application definition
 
